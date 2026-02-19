0e7c09d6bbe71b77dfb96ad3fd7cead21c3dbeb1
harunselam <harunselam97@gmail.com>
2026-02-19 20:30:34 +0100

    Fix WeatherManager URL and secure API key

--- Clima/Model/weatherManager.swift
+++ Clima/Model/weatherManager.swift
@@ -1,59 +1,60 @@
-
 import Foundation
 import CoreLocation
 
 protocol WeatherManagerDelegate {
-    func didUpdateWeather(_ weatherManager: WeatherManager,weather: WeatherModel)
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
     func didFailWithError(error: Error)
-    }
+}
 
 struct WeatherManager {
-    let weatherURL = "YOUR_API_KEY"
-    
-    func fetchWeather(cityName: String) {
-        let urlString = "\(weatherURL)&q=\(cityName)"
-        performRequest(with: urlString)
-        
-            }
-    func fetchWeather(latitude: CLLocationDegrees, longitute: CLLocationDegrees) {
-        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitute)"
-        performRequest(with: urlString)
-    }
+
+    private let baseURL = "https://api.openweathermap.org/data/2.5/weather"
+    private let apiKey = "YOUR_API_KEY"
+
     var delegate: WeatherManagerDelegate?
 
-    func performRequest(with urlString: String) {
-        if let url = URL(string: urlString) {
-            let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url) { data, response, error in
-                if error != nil {
-                    self.delegate?.didFailWithError(error: error!)
-                    return
-                }
-                if let safeData = data {
-                    if let weather = self.parseJSON(safeData) {
-                        self.delegate?.didUpdateWeather(self, weather: weather)
-                    }
-                }
-            }
-            task.resume()
-        }
-        
+    func fetchWeather(cityName: String) {
+        let urlString = "\(baseURL)?appid=\(apiKey)&units=metric&q=\(cityName)"
+        performRequest(with: urlString)
     }
-    func parseJSON(_ weatherData: Data) -> WeatherModel? {
+
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let urlString = "\(baseURL)?appid=\(apiKey)&units=metric&lat=\(latitude)&lon=\(longitude)"
+        performRequest(with: urlString)
+    }
+
+    private func performRequest(with urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                self.delegate?.didFailWithError(error: error)
+                return
+            }
+
+            guard let safeData = data else { return }
+
+            if let weather = self.parseJSON(safeData) {
+                self.delegate?.didUpdateWeather(self, weather: weather)
+            }
+        }
+        task.resume()
+    }
+
+    private func parseJSON(_ weatherData: Data) -> WeatherModel? {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
             let id = decodedData.weather[0].id
             let temp = decodedData.main.temp
             let name = decodedData.name
-            
-            let weather = WeatherModel(conditionID: id, cityName: name, temperature: temp)
-            return weather
-            
-            
+
+            return WeatherModel(conditionID: id, cityName: name, temperature: temp)
         } catch {
             delegate?.didFailWithError(error: error)
             return nil
         }
     }
 }
+
